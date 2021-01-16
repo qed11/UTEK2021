5b581c8a1b14f9ea14813917901815330a96a097
eqd11 <ericdai1130@gmail.com>
2021-01-16 13:46:27 -0500

    1a 1b update into functions

--- 1a/1a.py
+++ 1a/1a.py
@@ -1,21 +1,26 @@
 import json
-#load the json file
-f = open('1a.json','r')
-stations = json.load(f)
-f.close()
 
-#check accessibility and writ it into a list
-accessible = []
-for i in stations['Nodes']:
-    if i['Accessible'] == True:
-        accessible.append(i['Name'])
+def acc_check(filename:str):
+    #load the json file
+    f = open(filename,'r')
+    stations = json.load(f)
+    f.close()
 
-#write the list content into the .out file with the correct formatting
-wr = open('1a.out','w')
-for i in range(len(accessible)):
-    if i < len(accessible)-1:
-        wr.write(accessible[i])
-        wr.write(', ')
-    else:
-        wr.write(accessible[i])
-wr.close()
+    #check accessibility and writ it into a list
+    accessible = []
+    for i in stations['Nodes']:
+        if i['Accessible'] == True:
+            accessible.append(i['Name'])
+
+    #write the list content into the .out file with the correct formatting
+    wr = open('1a.out','w')
+    for i in range(len(accessible)):
+        if i < len(accessible)-1:
+            wr.write(accessible[i])
+            wr.write(', ')
+        else:
+            wr.write(accessible[i])
+    wr.close()
+
+if __name__ == '__main__':
+    acc_check('1a.json')

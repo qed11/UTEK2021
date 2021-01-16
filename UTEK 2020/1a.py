eaf574efd380867ccad7c266eb7956bbec6ef566
eqd11 <ericdai1130@gmail.com>
2021-01-16 11:33:29 -0500

    1a thingy

---
+++ UTEK 2020/1a.py
@@ -0,0 +1,18 @@
+import json
+f = open('1a.json','r')
+paths = json.load(f)
+f.close()
+yeet = []
+for i in paths['Nodes']:
+    if i['Accessible'] == True:
+        yeet.append(i['Name'])
+wr = open('1a.out','w')
+print(yeet[0])
+
+for i in range(len(yeet)):
+    if i < len(yeet)-1:
+        wr.write(yeet[i])
+        wr.write(', ')
+    else:
+        wr.write(yeet[i])
+wr.close()

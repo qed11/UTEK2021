2dd9e0425b309871638127e40c11d74543a4ba60
eqd11 <ericdai1130@gmail.com>
2021-01-16 12:19:35 -0500

    updated comments

--- 1a/1a.py
+++ 1a/1a.py
@@ -1,13 +1,17 @@
 import json
+#load the json file
 f = open('1a.json','r')
 stations = json.load(f)
 f.close()
+
+#check accessibility and writ it into a list
 yeet = []
 for i in stations['Nodes']:
     if i['Accessible'] == True:
         yeet.append(i['Name'])
 wr = open('1a.out','w')
 
+#write the list content into the .out file with the correct formatting
 for i in range(len(yeet)):
     if i < len(yeet)-1:
         wr.write(yeet[i])
--- 1b/1b.py
+++ 1b/1b.py
@@ -3,14 +3,16 @@ import json
 def myfunc(c):
     return c[1]
 
+#read the .json file
 f = open('1b.json', 'r')
 paths = json.load(f)
 sorts = []
 f.close()
 
+#check the accessibility ration by iterating through the Nodes contained in a path and tally the ones that are accessible
+#store the result in a list with format [PathName, accessibility to non-accessibility ratio]
 for i in paths['Paths']:
     access = 0
-    #print(i['PathName'])
     stations = i['Nodes']
     length = len(stations)
     for j in range(length):
@@ -19,8 +21,10 @@ for i in paths['Paths']:
     non = length - access
     sorts.append([i['PathName'],access/non])
 
-sorts.sort(reverse = True,key=myfunc)
+#using python's sorting algorithm to sort the 
+sorts.sort(reverse = True, key=myfunc)
 
+#write it out to 1b.out file
 k = 3
 writer = open('1b.out','w')
 for i in range(k):
@@ -29,3 +33,4 @@ for i in range(k):
         writer.write(', ')
     else:
         writer.write(sorts[i][0])
+writer.close()
